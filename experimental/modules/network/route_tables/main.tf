f47dd7ccb288ad0983e69ea14d7a45591cb360aa
cgoolsby <cgoolsby@uark.edu>
2019-01-26 13:04:08 -0800

    cleaned up routing tables, kinda redid it

--- experimental/modules/network/route_tables/main.tf
+++ experimental/modules/network/route_tables/main.tf
@@ -1,4 +1,4 @@
-resource "aws_default_route_table" "public-rt" {
+resource "aws_route_table" "public-rt" {
   vpc_id = "${var.vpc_id}"
   route {
     cidr_block = "0.0.0.0/0"
@@ -7,6 +7,9 @@ resource "aws_default_route_table" "public-rt" {
   tags {
     Name = "${terraform.workspace}-public-rt"
   }
+}
+resource "aws_default_route_table" "public-rt" {
+  default_route_table_id = "${aws_route_table.public-rt.id}"
 }
 #resource "aws_route_table" "private-rt" {
 #  vpc_id = "${var.vpc_id}"
@@ -18,11 +21,10 @@ resource "aws_default_route_table" "public-rt" {
 #    Name = "${terraform.workspace}-public-rt"
 #  }
 #}
-resource "aws_main_route_table_association" "public-rt" {
-  count = "${length(var.Public_Subnet_list)}"
-  subnet_id = "${var.Public_Subnet_list[count.index]}"
-  vpc_id = "${var.vpc_id}"
-  route_table_id = "${aws_route_table.public-rt.id}"
+resource "aws_route_table_association" "public-rt" {
+  count = "${length(var.Public_Subnet_id_list)}"
+  subnet_id = "${var.Public_Subnet_id_list[count.index]}"
+  route_table_id = "${aws_default_route_table.public-rt.id}"
 }
 
 #resource "aws_route_table" "private-rt" {
