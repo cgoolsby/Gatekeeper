be3096cdfb8dc6677022730b4e8222b605248e0e
cgoolsby <cgoolsby@uark.edu>
2019-01-25 19:09:07 -0800

    declared k8s module

--- experimental/main.tf
+++ experimental/main.tf
@@ -46,3 +46,12 @@ module "webservers" {
   github_link = "${var.github_link}"
   aws_key_name = "${var.key_name}"
 }
+module "k8s" {
+  source = "./modules/k8s/"
+  vpc_id = "${module.vpc_network.vpc_id}"
+  public_subnet_id = "${module.subnets.public_subnet_id}"
+  private_subnet_id = "${module.subnets.private_subnet_id}"
+  sg-BH_Cluster_Open_id = "${module.security_groups.sg-BH_Cluster_Open}"
+  sg-node_out_id = "${module.security_groups.sg-node_out}"
+  ### Where the tech params will be passed in
+}
