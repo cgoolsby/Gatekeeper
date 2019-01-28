2efa75d96af9d029648b9542eaa5a4b35a63009b
cgoolsby <cgoolsby@uark.edu>
2019-01-28 09:33:48 -0800

    added opening ports functionality, much thanks to stack overflow

--- CLI/main.go
+++ CLI/main.go
@@ -3,6 +3,8 @@ import (
   "fmt"
   "os"
   "log"
+  "strings"
+  "strconv"
 )
 
 
@@ -27,6 +29,64 @@ func check(e error) {
         panic(e)
     }
 }
+func askForConfirmation() bool {
+	var response string
+	_, err := fmt.Scanln(&response)
+	if err != nil {
+		log.Fatal(err)
+	}
+	okayResponses := []string{"y", "Y", "yes", "Yes", "YES"}
+	nokayResponses := []string{"n", "N", "no", "No", "NO"}
+	if containsString(okayResponses, response) {
+		return true
+	} else if containsString(nokayResponses, response) {
+		return false
+	} else {
+		fmt.Println("Please type yes or no and then press enter:")
+		return askForConfirmation()
+	}
+}
+// posString returns the first index of element in slice.
+// If slice does not contain element, returns -1.
+func posString(slice []string, element string) int {
+	for index, elem := range slice {
+		if elem == element {
+			return index
+		}
+	}
+	return -1
+}
+
+// containsString returns true iff slice contains element
+func containsString(slice []string, element string) bool {
+	return !(posString(slice, element) == -1)
+}
+func numbers(s string) []int {
+    var n []int
+    for _, f := range strings.Fields(s) {
+        i, err := strconv.Atoi(f)
+        if err == nil {
+            n = append(n, i)
+        }
+    }
+    return n
+}
+func portsOpen(name string) {
+  fmt.Println("Are there any specific ports you would like open for ", name)
+  if(askForConfirmation()) {
+    fmt.Println("Please type the ports you want open.  Seperate them with a space")
+    var portsString string
+    _, err := fmt.Scanln(&portsString)
+    if err != nil {
+      log.Fatal(err)
+    }
+    var n []int = numbers(portsString)
+    for _ , i := range n {
+      writeYml(fileName, "OpenPort", strconv.Itoa(i))
+    }
+  }
+}
+
 func writeYml(fileName, opt1, opt2 string) {
   write := (opt1 + ": " + opt2 + "\n")
   //write := []byte(opt1 + ": " + opt2 + "\n")
@@ -58,7 +118,7 @@ func main(){
   IngestionSystem := []string{"None", "Zookeeper.Kafka", "Pulsar"}
   name := "IngestionSystem"
   selectOption(name, IngestionSystem)
-  
+  portsOpen(name)
 
   //Compute Cluster
   ComputeCluster := []string{"None", "Hadoop.Spark", "Flink", "Presto", "Pelican"}
