7491715eedccf714ad568af3bca74d21c59983c9
cgoolsby <cgoolsby@uark.edu>
2019-01-30 08:39:30 -0800

    moved working_dir to correct place

--- experimental/modules/k8s_setup/database.tf
+++ experimental/modules/k8s_setup/database.tf
@@ -1,10 +1,10 @@
 resource "null_resource" "createDatabase" {
   count = "${var.database_exists}"
-  working_dir = "${path.module}"
   triggers = {
     discard = "${uuid()}"
   }
   provisioner "local-exec" {
+    working_dir = "${path.module}"
     command = "bash ${path.module}/runDatabase.sh ${var.Database}   ${var.DatabasePorts} PortEnd  ${var.DatabaseCopies}   ${var.DatabaseSize}"
 }
 }
