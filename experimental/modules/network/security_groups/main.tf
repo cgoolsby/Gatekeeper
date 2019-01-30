4d4b23d9b89b7b7249f4df5b627c7d0312392013
cgoolsby <cgoolsby@uark.edu>
2019-01-29 20:50:10 -0800

    fixed sg issue

--- experimental/modules/network/security_groups/main.tf
+++ experimental/modules/network/security_groups/main.tf
@@ -26,8 +26,8 @@ resource "aws_security_group_rule" "open_self_in" {
   to_port = 0
   protocol = "-1"
   security_group_id = "${aws_security_group.BH_Cluster_Open.id}"
-  source_security_group_id = "${aws_security_group.BH_Cluster_Open.id}"
- # cidr_blocks = ["0.0.0.0/0"]
+ # source_security_group_id = "${aws_security_group.BH_Cluster_Open.id}"
+  cidr_blocks = ["0.0.0.0/0"]
 }
 resource "aws_security_group_rule" "open_self_out" {
   type = "egress"
@@ -35,8 +35,8 @@ resource "aws_security_group_rule" "open_self_out" {
   to_port = 0
   protocol = "-1"
   security_group_id = "${aws_security_group.BH_Cluster_Open.id}"
-  source_security_group_id = "${aws_security_group.BH_Cluster_Open.id}"
-  #cidr_blocks = ["0.0.0.0/0"]
+  #source_security_group_id = "${aws_security_group.BH_Cluster_Open.id}"
+  cidr_blocks = ["0.0.0.0/0"]
 }
 resource "aws_security_group_rule" "ssh_in" {
   type = "ingress"
