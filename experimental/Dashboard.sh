fe68c49d896ace88dab884ae0c7f22600ce69a41
cgoolsby <cgoolsby@uark.edu>
2019-02-16 09:55:30 -0800

    kubernetes dashboard improvement

--- experimental/Dashboard.sh
+++ experimental/Dashboard.sh
@@ -1,5 +1,6 @@
-kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended/kubernetes-dashboard.yaml
-kubectl proxy &
+#kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended/kubernetes-dashboard.yaml
+kubectl proxy --port=8080 --address='0.0.0.0' --disable-filter=true &
 
 aws-iam-authenticator token -i cluster-name
-echo "Copy and paste the Token into the kubernetes webpage: http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/"
+echo "Copy and paste the Token into the kubernetes webpage: http://localhost:8080"  #/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/"
+#kubectl proxy
