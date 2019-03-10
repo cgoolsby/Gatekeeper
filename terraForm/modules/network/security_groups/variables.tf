fb496ee31e6e1e4e34c6c6ee3071509e2cf36223
cgoolsby <cgoolsby@uark.edu>
2019-03-10 08:24:31 -0700

    added ingress only from ip that runs terraform init

--- terraForm/modules/network/security_groups/variables.tf
+++ terraForm/modules/network/security_groups/variables.tf
@@ -1,3 +1,11 @@
 variable "vpc_id" {
   description = "the vpc networks id"
 }
+data "http" "ip-address" {
+  url = "http://ipv4.icanhazip.com"
+}
+
+
+variable "home-ip" {
+  default = ${chomp(data.http.ip-address.body)}/32"
+}
