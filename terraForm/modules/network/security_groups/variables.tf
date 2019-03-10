5a23f33b0fc1d96c00ee5d639ceae9f29b28d069
cgoolsby <cgoolsby@uark.edu>
2019-03-10 09:11:56 -0700

    fixed typo

--- terraForm/modules/network/security_groups/variables.tf
+++ terraForm/modules/network/security_groups/variables.tf
@@ -1,11 +1,11 @@
 variable "vpc_id" {
   description = "the vpc networks id"
 }
+
 data "http" "ip-address" {
   url = "http://ipv4.icanhazip.com"
 }
 
-
 variable "home-ip" {
-  default = ${chomp(data.http.ip-address.body)}/32"
+  default = "${chomp(data.http.ip-address.body)}/32"
 }
