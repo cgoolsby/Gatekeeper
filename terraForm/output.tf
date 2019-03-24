cb9d76580f7681f2c68bacd1b4334af371f8889e
Curtis Goolsby <Curtis@Admins-MacBook-Pro.local>
2019-03-24 10:15:06 -0700

    directions for enabling monitoring solutions

--- terraForm/output.tf
+++ terraForm/output.tf
@@ -4,3 +4,6 @@ output "config_map_aws_auth" {
 output "kubeconfig" {
     value = "${module.k8s.kubeconfig}"
 }
+output "Monitoring" {
+  value = "To enable Monitoring, run the bash scripts Prometheus.sh and Dashboard.sh."
+}
