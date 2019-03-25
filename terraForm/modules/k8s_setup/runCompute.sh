162929625cd7238fb565c2f1ce69b61b47fbd524
Curtis Goolsby <Curtis@Admins-MacBook-Pro.local>
2019-03-24 19:19:31 -0700

    working version of master/worker # options for spark

--- terraForm/modules/k8s_setup/runCompute.sh
+++ terraForm/modules/k8s_setup/runCompute.sh
@@ -26,20 +26,19 @@ workerCPUMin=$1; shift
 workerMemMax=$1; shift
 workerMemMin=$1; shift
 
-rm -rf compute/current/
-mkdir compute/current/
+rm -rf compute/current/*
 cp -r compute/$name/* compute/current/ 
 
-for i in `compute/current/*.yml`; do echo $i; sed -i'' 's/MASTER/$master/g' $i; done
-for i in `compute/current/*.yml`; do echo $i; sed -i'' 's/WORKER/$worker/g' $i; done
-for i in `compute/current/*.yml`; do echo $i; sed -i'' 's/MASTERCPUMAX/$masterCPUMax/g' $i; done
-for i in `compute/current/*.yml`; do echo $i; sed -i'' 's/MASTERCPUMIN/$masterCPUMin/g' $i; done
-for i in `compute/current/*.yml`; do echo $i; sed -i'' 's/MASTERMEMMAX/$masterMemMax/g' $i; done
-for i in `compute/current/*.yml`; do echo $i; sed -i'' 's/MASTERMEMMIN/$masterMemMin/g' $i; done
-for i in `compute/current/*.yml`; do echo $i; sed -i'' 's/WORKERCPUMAX/$workerCPUMax/g' $i; done
-for i in `compute/current/*.yml`; do echo $i; sed -i'' 's/WORKERCPUMIN/$workerCPUMin/g' $i; done
-for i in `compute/current/*.yml`; do echo $i; sed -i'' 's/WORKERMEMMAX/$workerMemMax/g' $i; done
-for i in `compute/current/*.yml`; do echo $i; sed -i'' 's/WORKERMEMMIN/$workerMemMin/g' $i; done
+for i in `ls compute/current/*.yml`; do echo $i; sed -i '' "s/MASTER/$master/g" $i; done
+for i in `ls compute/current/*.yml`; do echo $i; sed -i '' "s/WORKER/$worker/g" $i; done
+for i in `ls compute/current/*.yml`; do echo $i; sed -i '' "s/MASTERCPUMAX/$masterCPUMax/g" $i; done
+for i in `ls compute/current/*.yml`; do echo $i; sed -i '' "s/MASTERCPUMIN/$masterCPUMin/g" $i; done
+for i in `ls compute/current/*.yml`; do echo $i; sed -i '' "s/MASTERMEMMAX/$masterMemMax/g" $i; done
+for i in `ls compute/current/*.yml`; do echo $i; sed -i '' "s/MASTERMEMMIN/$masterMemMin/g" $i; done
+for i in `ls compute/current/*.yml`; do echo $i; sed -i '' "s/WORKERCPUMAX/$workerCPUMax/g" $i; done
+for i in `ls compute/current/*.yml`; do echo $i; sed -i '' "s/WORKERCPUMIN/$workerCPUMin/g" $i; done
+for i in `ls compute/current/*.yml`; do echo $i; sed -i '' "s/WORKERMEMMAX/$workerMemMax/g" $i; done
+for i in `ls compute/current/*.yml`; do echo $i; sed -i '' "s/WORKERMEMMIN/$workerMemMin/g" $i; done
 for i in `ls compute/current/*.yml`; do kubectl create -f $i; done
 #Port Forwarding Logic here? i.e. for loop through the array, open ports.  Seems difficult to target each instance after creation?
 
